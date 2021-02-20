599faa18d0eddab139feebf4114294e009f3add4
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-02-20 22:52:17 +0530

    quick sort

---
+++ Searching-Sorting/8-quick-sort.js
@@ -0,0 +1,30 @@
+function swap(arr, i, j) {
+  return ([arr[i], arr[j]] = [arr[j], arr[i]]);
+}
+
+function pivot(arr, start, end) {
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([4, 8, 76, 1, 3, -10, 25, 9, 5]));
